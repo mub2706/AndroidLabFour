7dc33e1d88920ae4d600620455c54d35c5eca71c
mub2706 <144853741+mub2706@users.noreply.github.com>
2023-10-16 22:43:23 -0400

    Final Submission for lab 4

--- app/src/main/java/com/cst3104/androidlab4/SecondActivity.java
+++ app/src/main/java/com/cst3104/androidlab4/SecondActivity.java
@@ -1,24 +1,19 @@
 package com.cst3104.androidlab4;
 
-import android.Manifest;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -27,111 +22,56 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Objects;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private TextView textView3;
     private EditText editTextPhone;
-    private Button callButton;
-    private ImageButton imageButton;
     private ImageView myImageView;
-    private static final int REQUEST_CALL_PERMISSION = 1;
 
-    private ActivityResultLauncher<Intent> cameraResult;
+    private Button button3;
+    private static final int REQUEST_CALL_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        textView3 = findViewById(R.id.textView3);
+        TextView textView3 = findViewById(R.id.textView3);
         editTextPhone = findViewById(R.id.editTextPhone);
-        callButton = findViewById(R.id.button2);
-        imageButton = findViewById(R.id.imageButton);
+        Button callButton = findViewById(R.id.button2);
+        myImageView = findViewById(R.id.myImageView);
+        button3 = findViewById(R.id.button3);
+
 
         Intent fromPrevious = getIntent();
         String emailAddress = fromPrevious.getStringExtra("EmailAddress");
 
+        TextView welcomeMsg = findViewById(R.id.textView3);
+        String displayMsg = "Welcome back " + emailAddress;
+        welcomeMsg.setText(displayMsg);
 
         int MY_PERMISSIONS_REQUEST_CAMERA=0;
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             if (!ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CAMERA)) {
                 ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA );
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         }
-        // Retrieve data from Shared Preferences
-        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
 
-
-        // Set the retrieved email address in TextView
-        textView3.setText(emailAddress);
-
-        setCaptureImage();
-
-        cameraResult = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                            if (data != null) {
-                                Bitmap thumbnail = (Bitmap) data.getParcelableExtra("data");
-                                if (thumbnail != null) {
-                                    imageButton.setImageBitmap(thumbnail);
-                                    saveImageToInternalStorage(thumbnail, "Picture.png");
-
-                                    imageButton.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View view) {
-                                            // Launch the camera intent
-                                            setCaptureImage();
-                                        }
-                                    });
-                                }
-                            }
-                        }
-                    }
-                });
-
-        callButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String phoneNumber = editTextPhone.getText().toString();
-                Log.d("ButtonClick", "Change Picture button clicked");
-
-                if (ContextCompat.checkSelfPermission(SecondActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-                    makePhoneCall(phoneNumber);
-                } else {
-                    ActivityCompat.requestPermissions(SecondActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PERMISSION);
-                }
-            }
-        });
-
-        // Load and display the saved image from internal storage
-        Bitmap savedImage = loadImageFromInternalStorage("Picture.png");
-        if (savedImage != null) {
-            myImageView.setImageBitmap(savedImage);
-        }
-    }
-
-        private void makePhoneCall(String phoneNumber) {
+        editTextPhone.setOnClickListener(view -> {
+            String phoneNumber = editTextPhone.getText().toString();
             Intent callIntent = new Intent(Intent.ACTION_CALL);
             callIntent.setData(Uri.parse("tel:" + phoneNumber));
             startActivity(callIntent);
-    }
+        });
 
-    private void setCaptureImage() {
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
         ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
@@ -140,43 +80,61 @@ public class SecondActivity extends AppCompatActivity {
                         Intent data = result.getData();
                         if (data != null) {
                             Bitmap thumbnail = (Bitmap) data.getParcelableExtra("data");
-                            if (thumbnail != null) {
-                                // Call setImageBitmap on the instance of ImageButton
-                                imageButton.setImageBitmap(thumbnail);
-                                saveImageToInternalStorage(thumbnail, "Picture.png");
-                            } else {
-                                Log.e("CaptureImage", "Bitmap is null");
+                            myImageView.setImageBitmap(thumbnail);
+
+                            FileOutputStream fOut = null;
+                            try {
+                                fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                                Objects.requireNonNull(thumbnail).compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                fOut.flush();
+                                fOut.close();
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
-                        } else {
-                            Log.e("CaptureImage", "Intent data is null");
                         }
-                    } else {
-                        Log.e("CaptureImage", "Capture result is not OK");
                     }
-                });
+                }
+        );
 
-        cameraResult.launch(cameraIntent);
-    }
+        callButton.setOnClickListener(view -> {
+            String phoneNumber = editTextPhone.getText().toString();
+            Log.d("ButtonClick", "Change Picture button clicked");
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            callIntent.setData(Uri.parse("tel:" + phoneNumber));
+            startActivity(callIntent);
+        });
 
-    private void saveImageToInternalStorage(Bitmap bitmap, String filename) {
-        FileOutputStream fOut = null;
-        try {
-            fOut = openFileOutput(filename, Context.MODE_PRIVATE);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
-            fOut.flush();
-            fOut.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Bitmap loadImageFromInternalStorage(String filename) {
-        File file = new File(getFilesDir(), filename);
+        String filename = "Picture.png";
+        File file = new File(getExternalFilesDir(null), filename);
         if (file.exists()) {
-            return BitmapFactory.decodeFile(file.getAbsolutePath());
+            Bitmap theImage;
+            theImage = BitmapFactory.decodeFile(file.getAbsolutePath());
+            myImageView.setImageBitmap(theImage);
         }
-        return null;
+
+        button3.setOnClickListener(view -> {
+            Log.w("SecondActivity", "Before Camera Launch");
+            cameraResult.launch(cameraIntent);
+        });
+
+        SharedPreferences prefs = getSharedPreferences("MyData", MODE_PRIVATE);
+        String savedNumber = prefs.getString("EmailAddress", "");
+        textView3.setText(emailAddress);
+        }
+
+        protected void onPause() {
+            super.onPause();
+            EditText phoneText;
+            phoneText = findViewById(R.id.editTextPhone);
+            String phone = phoneText.getText().toString();
+
+            SharedPreferences prefs = getSharedPreferences("MyData", MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+
+            editor.putString("PhoneNumber", phone);
+            editor.apply();
+        }
     }
-}
+
